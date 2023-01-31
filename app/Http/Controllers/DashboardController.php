5432eb6050cfb3e3ce17b078fb943cae9776a552
Kenneth Ryan <dykennethryan@gmail.com>
2023-01-31 10:33:00 +0800

    update dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -17,11 +17,6 @@ class DashboardController extends Controller
 {
 	public function index() {
 		$user = auth()->user();
-		$now = Carbon::now();
-
-		$tbt = ToolboxTalk::select(DB::raw("COUNT(`tbt_id`) as total_tbt"))
-		->where("tbl_toolbox_talks.is_deleted", 0);
-
 			
 		return Inertia::render("Dashboard/General/Analytics/index", [
 			"employeesCount" => Employee::where("is_deleted", 0)->where("sub_id", $user->subscriber_id)->count(),
