4e4c6fed8a2285cc413fa145c3a43f3f66277253
Kenneth Ryan <dykennethryan@gmail.com>
2023-03-30 05:10:53 +0800

    update role and permissions seeder

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -92,6 +92,74 @@ class PermissionSeeder extends Seeder
       // gets all permissions via Gate::before rule; see AuthServiceProvider
 			Role::create(['name' => 'Admin']);
 
+			$modPermissions = [
+				'permission_access',
+				'permission_create',
+				'permission_edit',
+				'permission_delete',
+
+				'image_upload_slider',
+				'image_upload_defaultCover',
+				'image_upload_delete',
+
+				'role_access',
+				'role_create',
+				'role_edit',
+				'role_delete',
+
+				'user_access',
+				'user_create',
+				'user_edit',
+				'user_show',
+				'user_delete',
+
+				'employee_create',
+				'employee_delete',
+				'employee_edit',
+				'employee_show',
+
+				'training_create',
+				'training_edit',
+				'training_delete',
+				'training_show',
+
+				'inspection_create',
+				'inspection_edit',
+				'inspection_delete',
+				'inspection_show',
+
+				'talk_toolbox_create',
+				'talk_toolbox_edit',
+				'talk_toolbox_delete',
+				'talk_toolbox_show',
+
+				'folder_create',
+				'folder_edit',
+				'folder_delete',
+				'folder_show',
+				
+				'file_create',
+				'file_edit',
+				'file_delete',
+				'file_show',
+
+				'inventory_create',
+				'inventory_edit',
+				'inventory_delete',
+				'inventory_show',
+
+				'stock_create',
+				'stock_edit',
+				'stock_delete',
+				'stock_show',
+			];
+
+			$modRole = Role::create(['name' => 'Moderator']);
+
+			foreach ($modPermissions as $permission)   {
+				$modRole->givePermissionTo($permission);
+			}
+
 			$admin = User::where('user_type', 0)->first();
 			$admin->assignRole("Admin");
 
