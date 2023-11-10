31b67ba75b9abfd10ea6a47e3a49b9ad6417e11a
Kenneth Ryan <dykennethryan@gmail.com>
2023-11-10 15:04:51 +0800

    remove 503 to handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -51,7 +51,7 @@ class Handler extends ExceptionHandler
 
 	public function render($request, Throwable $e) {
 		$response = parent::render($request, $e);
-		if (!app()->environment(['local', 'testing', 'development']) && in_array($response->status(), [500, 503, 404, 403])) {
+		if (!app()->environment(['local', 'testing', 'development']) && in_array($response->status(), [500, 404, 403])) {
 			switch ($response->status()) {
 				case 500:
 					$page = 'Page500';
