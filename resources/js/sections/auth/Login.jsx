d509bf067ffbd35e5166a8272866d6e1a24bbd9b
kennethryandy <dykennethryan@gmail.com>
2024-03-14 20:54:27 +0800

    update image

--- resources/js/sections/auth/Login.jsx
+++ resources/js/sections/auth/Login.jsx
@@ -1,43 +1,61 @@
 // @mui
-import { Link } from '@inertiajs/inertia-react';
-import { Stack, Typography, Link as MuiLink, Box } from '@mui/material';
-import Image from '@/Components/image';
+import { Link } from "@inertiajs/inertia-react";
+import { Stack, Typography, Link as MuiLink, Box } from "@mui/material";
+import Image from "@/Components/image";
 // layouts
-import LoginLayout from '@/Layouts/login/LoginLayout';
+import LoginLayout from "@/Layouts/login/LoginLayout";
 //
-import AuthLoginForm from './AuthLoginForm';
+import AuthLoginForm from "./AuthLoginForm";
 // import AuthWithSocial from './AuthWithSocial';
 
 // ----------------------------------------------------------------------
 
-export default function Login () {
+export default function Login() {
+    return (
+        <LoginLayout>
+            <Box sx={{ display: "flex", justifyContent: "center", mb: 3 }}>
+                <Image
+                    disabledEffect
+                    visibleByDefault
+                    alt="Fiafi"
+                    src={route("image", { path: "assets/Fiafi-logo.png" })}
+                    sx={{ maxWidth: 140, "& img": { height: "auto" } }}
+                />
+            </Box>
+            <Stack spacing={2} sx={{ mb: 5, position: "relative" }}>
+                <Typography variant="h5">
+                    Sign in to <br />{" "}
+                    <Box component="span" sx={{ color: "primary.main" }}>
+                        I
+                    </Box>
+                    ntegrated{" "}
+                    <Box component="span" sx={{ color: "primary.main" }}>
+                        M
+                    </Box>
+                    anagement{" "}
+                    <Box component="span" sx={{ color: "primary.main" }}>
+                        S
+                    </Box>
+                    ystem
+                </Typography>
 
-	return (
-		<LoginLayout>
-			<Box sx={{ display: "flex", justifyContent: "center", mb: 3 }}>
-				<Image
-					disabledEffect
-					visibleByDefault
-					alt="Fiafi"
-					src={'/storage/assets/Fiafi-logo.png'}
-					sx={{ maxWidth: 140, "& img": { height: "auto" } }}
-				/>
-			</Box>
-			<Stack spacing={2} sx={{ mb: 5, position: 'relative' }}>
-				<Typography variant="h5">Sign in to <br /> <Box component="span" sx={{ color: "primary.main" }}>I</Box>ntegrated <Box component="span" sx={{ color: "primary.main" }}>M</Box>anagement <Box component="span" sx={{ color: "primary.main" }}>S</Box>ystem</Typography>
+                <Stack direction="row" spacing={0.5}>
+                    <Typography variant="body2">New user?</Typography>
 
-				<Stack direction="row" spacing={0.5}>
-					<Typography variant="body2">New user?</Typography>
+                    <MuiLink
+                        component={Link}
+                        preserveScroll
+                        href="/register"
+                        variant="subtitle2"
+                    >
+                        Create an account
+                    </MuiLink>
+                </Stack>
+            </Stack>
 
-					<MuiLink component={Link} preserveScroll href="/register" variant="subtitle2">
-						Create an account
-					</MuiLink>
-				</Stack>
-			</Stack>
+            <AuthLoginForm />
 
-			<AuthLoginForm />
-
-			{/* <AuthWithSocial /> */}
-		</LoginLayout>
-	);
+            {/* <AuthWithSocial /> */}
+        </LoginLayout>
+    );
 }
