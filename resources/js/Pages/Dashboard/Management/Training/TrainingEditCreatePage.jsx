f5b736b2d8cc1c02c0d5a1b1969cf8815f400b70
kennethryandy <dykennethryan@gmail.com>
2024-09-04 04:40:37 +0800

    remove induction to create training

--- resources/js/Pages/Dashboard/Management/Training/TrainingEditCreatePage.jsx
+++ resources/js/Pages/Dashboard/Management/Training/TrainingEditCreatePage.jsx
@@ -1,52 +1,68 @@
-
 // @mui
-import { Container } from '@mui/material';
+import { Container } from "@mui/material";
 // routes
-import { PATH_DASHBOARD } from '@/routes/paths';
+import { PATH_DASHBOARD } from "@/routes/paths";
 // components
-import { useSettingsContext } from '@/Components/settings';
-import CustomBreadcrumbs from '@/Components/custom-breadcrumbs';
+import { useSettingsContext } from "@/Components/settings";
+import CustomBreadcrumbs from "@/Components/custom-breadcrumbs";
 // sections
-import TrainingNewEditForm from '@/sections/@dashboard/training/form';
-import { Head } from '@inertiajs/inertia-react';
+import TrainingNewEditForm from "@/sections/@dashboard/training/form";
+import { Head } from "@inertiajs/inertia-react";
 
 // ----------------------------------------------------------------------
 
 const TYPE_OPTIONS = {
-	2: "client",
-	3: "thirdParty",
-	4: "induction"
+    2: "client",
+    3: "thirdParty",
+    // 4: "induction"
 };
 
-export default function TrainingEditCreatePage ({ isEdit = false, details = null, currentTraining, type, projectDetails }) {
-	const { themeStretch } = useSettingsContext();
+export default function TrainingEditCreatePage({
+    isEdit = false,
+    details = null,
+    currentTraining,
+    type,
+    projectDetails,
+}) {
+    const { themeStretch } = useSettingsContext();
 
-	return (
-		<>
-			<Head>
-				<title>{isEdit ? 'Update course' : 'New training'}</title>
-			</Head>
+    return (
+        <>
+            <Head>
+                <title>{isEdit ? "Update course" : "New training"}</title>
+            </Head>
 
-			<Container maxWidth={themeStretch ? false : 'lg'}>
-				<CustomBreadcrumbs
-					heading={isEdit ? 'Update course' : 'New training'}
-					links={[
-						{
-							name: 'Dashboard',
-							href: PATH_DASHBOARD.root,
-						},
-						{
-							name: details ? `${details?.title} List` : 'List',
-							href: (isEdit && details) ? PATH_DASHBOARD.training[details?.url] : type ? PATH_DASHBOARD.training[TYPE_OPTIONS[type]] : PATH_DASHBOARD.training.client,
-						},
-						{
-							name: isEdit ? 'Update course' : 'New training',
-						},
-					]}
-				/>
+            <Container maxWidth={themeStretch ? false : "lg"}>
+                <CustomBreadcrumbs
+                    heading={isEdit ? "Update course" : "New training"}
+                    links={[
+                        {
+                            name: "Dashboard",
+                            href: PATH_DASHBOARD.root,
+                        },
+                        {
+                            name: details ? `${details?.title} List` : "List",
+                            href:
+                                isEdit && details
+                                    ? PATH_DASHBOARD.training[details?.url]
+                                    : type
+                                    ? PATH_DASHBOARD.training[
+                                          TYPE_OPTIONS[type]
+                                      ]
+                                    : PATH_DASHBOARD.training.client,
+                        },
+                        {
+                            name: isEdit ? "Update course" : "New training",
+                        },
+                    ]}
+                />
 
-				<TrainingNewEditForm isEdit={isEdit} currentTraining={currentTraining} projectDetails={projectDetails} />
-			</Container>
-		</>
-	);
+                <TrainingNewEditForm
+                    isEdit={isEdit}
+                    currentTraining={currentTraining}
+                    projectDetails={projectDetails}
+                />
+            </Container>
+        </>
+    );
 }
