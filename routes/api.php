51726cbbcb19277f95a634f2509b1cc5ed21d7e9
Kieffer <kganza@traxiontech.net>
2025-06-23 21:13:34 +0800

    fix

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth')->as('api.')->group(function ()
+Route::group(function ()
 {
 
 	Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
